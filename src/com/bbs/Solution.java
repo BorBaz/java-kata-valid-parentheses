13963a8ee55461e3265b087f44ec2af522f0440a
Borja <boorjabs@gmail.com>
2022-08-16 13:45:17 +0200

    Solution

--- src/com/bbs/Solution.java
+++ src/com/bbs/Solution.java
@@ -1,9 +1,27 @@
 package com.bbs;
 
-public class Solution{
+public class Solution {
 
-    public static boolean validParentheses(String parens)
-    {
-        //Put code below
+    public static boolean validParentheses(String parens) {
+
+        int v = 0;
+        int r = 0;
+
+        char[] k = parens.toCharArray();
+
+        for (char c : k) {
+
+            if (c == ('(')) {
+                r++;
+            } else if (c == (')')) {
+                v++;
+            }
+
+            if (v > r) {
+                break;
+            }
+        }
+
+        return v == r;
     }
 }
